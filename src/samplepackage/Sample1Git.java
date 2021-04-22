2146f003269301cdfa20d0069fb309aaefb6176e
srihari <srihari@LAPTOP-4SEVEBP1>
2021-04-22 20:02:32 +0530

    first commit....

---
+++ src/samplepackage/Sample1Git.java
@@ -0,0 +1,15 @@
+package samplepackage;
+
+public class Sample1Git {
+
+	public static void main(String[] args) {
+		String s = "hai";
+
+		if (s == "hai") {
+
+			System.out.println("say hai");
+		} else {
+			System.out.println("byeee");
+		}
+	}
+}
